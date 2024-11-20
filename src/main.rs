9734c30a5c1e8aa4439a8277c71433f9e0512ac6
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-20 23:13:18 +0000

    Add minimal printing to screen to check setup's correctness

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello, world!";
+static VGA_BUFFER_START: u32 = 0xB8000;
+static LIGHT_CYAN: u8 = 0xB;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = VGA_BUFFER_START as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = LIGHT_CYAN;
+        }
+    }
+
     loop {}
 }
 
